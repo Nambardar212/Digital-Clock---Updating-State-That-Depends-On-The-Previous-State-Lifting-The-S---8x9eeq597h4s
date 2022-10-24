a225a141640e345dd4638f7fd498c5f888c8c70c
Nambardar212 <nikhilsaini0210@gmail.com>
2022-10-24 21:56:04 +0530

    1st

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 const App = () => {
 
+  const [time, setTime] = useState('')
+ 
+
+  // console.log(new Date().toLocaleString())
+ 
+  useEffect(()=> {
+    setTimeout(() => {
+      var t = new Date().toLocaleString()
+      setTime(t)
+    },1000)
+  },[time])
+
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">
+        {time}
+      </div>
     </div>
   )
 }
